753ee5870664da29dce5835e7af18e118d8ea22c
Anas TAHER <taheranas24@gmail.com>
2023-03-30 13:18:13 +0200

    Task 101 commit v1

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - generates valid passwords for the program 101-crackme
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	printf("zzzzzzzzzzzzzzzzzzzzzzX");
+
+	return (0);
+}
