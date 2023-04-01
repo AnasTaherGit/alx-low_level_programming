f9a216d50a5d3686cf5d1d37fa8894812a305b8f
Anas TAHER <taheranas24@gmail.com>
2023-04-01 18:38:41 +0200

    task 0 betty conformity

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -13,17 +13,16 @@ char *_strcat(char *dest, char *src)
 {
 	int i;
 
-	int destlen=strlen(dest);
-	
-	int srclen=strlen(src);
+	int destlen = strlen(dest);
 
-	for (i=0; i < srclen ; i++)
+	int srclen = strlen(src);
+
+	for (i = 0; i < srclen ; i++)
 	{
-		
-		dest[destlen+i] = src[i];
+		dest[destlen + i] = src[i];
 	}
 
 	dest[destlen + srclen] = '\0';
 
-	return dest;
+	return (dest);
 }
