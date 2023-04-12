dac1e8cb0baa0453b4de3b685a24dd2715c51e7a
Anas TAHER <taheranas24@gmail.com>
2023-04-12 22:42:15 +0200

    Project 0x07 setup

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strncpy - Copy from dest to src
+ * @dest: Destination 
+ * @src: Source
+ * @n: Length to copy
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	
+}
