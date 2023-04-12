aba1453f3f66e5a7bb4b9036e1a288fe686196cc
Anas TAHER <taheranas24@gmail.com>
2023-04-13 00:06:58 +0200

    Task 4 fix

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -8,6 +8,19 @@
  * Return: 0
  */
 
+int _isdigit(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] < '0' || s[i] > '9')
+			return (0);
+	}
+
+	return (1);
+}
+
 int main(int argc, char *argv[])
 {
 	int i;
@@ -15,7 +28,7 @@ int main(int argc, char *argv[])
 
 	for (i = 1; i < argc; i++)
 	{
-		if (atoi(argv[i]) == 0 && argv[i][0] != '0')
+		if (_isdigit(argv[i]) == 0)
 		{
 			printf("Error\n");
 			return (1);
