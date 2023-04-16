0282cc316ed54ea0fe83256080e7dd2ae1db4d6c
Anas TAHER <taheranas24@gmail.com>
2023-04-16 13:59:44 +0200

    Task 2 commit

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,13 +1,25 @@
 #include "main.h"
-
+#include <string.h>
 /**
  * _strncpy - Copy from dest to src
- * @dest: Destination 
+ * @dest: Destination
  * @src: Source
  * @n: Length to copy
  */
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	
+	int i;
+
+	int srclen = strlen(src);
+
+	if (srclen > n)
+		srclen = n;
+
+	for (i = 0; i < srclen; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	return (dest);
 }
