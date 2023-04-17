20d5a44e1bfe32a67f83b5ed60bc810a265a3e46
Anas TAHER <taheranas24@gmail.com>
2023-04-17 16:33:50 +0200

    Task 10 commit

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -2,16 +2,16 @@
 
 int main(void)
 {
-  int n;
-  int a[5];
-  int *p;
+	int n;
+	int a[5];
+	int *p;
 
-  a[2] = 1024;
-  p = &n;
+	a[2] = 1024;
+	p = &n;
 
-  /* Add the line below to set a[2] to 98 */
-  *(a + 2) = 98;
-  /* ...so that this prints 98\n */
-  printf("a[2] = %d\n", a[2]);
-  return (0);
+	/* Add the line below to set a[2] to 98 */
+	*(p + 6) = 98;
+	/* ...so that this prints 98\n */
+	printf("a[2] = %d\n", a[2]);
+	return (0);
 }
