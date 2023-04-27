5f40e8746016f9b21efcbd5a1e098640eb794507
Anas TAHER <taheranas24@gmail.com>
2023-04-28 00:16:43 +0200

    Task 0 commit

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,12 +9,15 @@
 
 size_t print_list(const list_t *h)
 {
-	int i;
-	list_t *current = h;
+	int i = 0;
+	list_t *current = (list_t *)h;
 
 	while (current != NULL)
 	{
-		printf("[%d] %s\n", current->len, current->str);
+		if (current->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", current->len, current->str);
 		current = current->next;
 		i++;
 	}
