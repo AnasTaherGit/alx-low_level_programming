7ec810b44d408f11e5f85959a87a86d52e36c0ae
Anas TAHER <taheranas24@gmail.com>
2023-04-28 01:11:29 +0200

    Task 4 commit

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * free_list - frees a list_t list
+ * @head: pointer to head of list_t list
+ *
+ * Return: void
+ */
+
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	if (head == NULL)
+		return;
+
+	while (head != NULL)
+	{
+		temp = head;
+		head = head->next;
+		free(temp->str);
+		free(temp);
+	}
+}
