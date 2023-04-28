c9845e61b9ee1145e048cc72e38c7257cdc78d14
Anas TAHER <taheranas24@gmail.com>
2023-04-28 02:10:57 +0200

    Task 100 fix v2

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
+/**
+ * first_main - prints a string before the
+ *              main function is executed.
+ *
+ * Return: Always void.
+ */
+
 void __attribute__((constructor)) first_main(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\nI bore\
+	 my house upon my back!\n");
 }
