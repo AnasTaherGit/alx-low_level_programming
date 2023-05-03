cf63d05bb6400638b4113d81f999193fcf6d49eb
Anas TAHER <taheranas24@gmail.com>
2023-05-03 01:45:34 +0100

    Task 3 fix v1

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -15,7 +15,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
 	{
-		return NULL;
+		return (NULL);
 	}
 
 	new_node->n = n;
@@ -35,5 +35,5 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 		current_node->next = new_node;
 	}
 
-	return new_node;
+	return (new_node);
 }
